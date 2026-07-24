7433a068f775428400b8bb2a6be89a3aa69ac4e0
pillipwipwit <cedricsenez@gmail.com>
2026-07-24 14:56:44 +0200

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://www.cdsbf77.org',
-  integrations: [sitemap()],
+  integrations: [sitemap(), react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
